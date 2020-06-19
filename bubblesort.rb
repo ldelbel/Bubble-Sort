7c5805a84abac193fe3e2a8fe731fcbf5125b76d
ldelbel <lucdelbel@gmail.com>
2020-06-19 06:58:34 -0300

    Fix spacing rubocop errors

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,10 +1,10 @@
 arr = [11, 6, 2, 100, 20, 2, 1, 9, 5, 10, 11]
 def bubble_sort(arr)
   i = 0
-  while i < arr.length - 1 
-    if arr[i] > arr[i + 1] 
+  while i < arr.length - 1
+    if arr[i] > arr[i + 1]
       arr[i], arr[i + 1] = arr[i + 1], arr[i]
-      i -= 1 unless i.zero?        
+      i -= 1 unless i.zero?
     else
       i += 1
     end
@@ -16,17 +16,17 @@ bubble_sort(arr)
 def bubble_sort_by(arr)
   i = 0
   while i < arr.length - 1
-    if yield(arr[i],arr[i + 1]) > 0
+    if yield(arr[i], arr[i + 1]).positive?
       arr[i], arr[i + 1] = arr[i + 1], arr[i]
       i -= 1 unless i.zero?  
     else
       i += 1
     end
-  end  
+  end
   puts arr.to_s
 end
 
-array = ['how are you','hi','Hi Jane how are you','hello','Jane', 'Lucas']
+array = ['how are you', 'hi', 'Hi Jane how are you', 'hello', 'Jane', 'Lucas']
 
 bubble_sort_by(array) do |left,right|
   left.length - right.length
